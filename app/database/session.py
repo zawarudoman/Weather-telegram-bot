7c1b4c807bbc0c00370e9cb9864ea88ff8de2b02
Максим Лесков <max.leskoff@icloud.com>
2025-11-10 22:31:04 +0300

    add database session configuration

---
+++ app/database/session.py
@@ -0,0 +1,26 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./weather_bot.db")
+
+if DATABASE_URL.startswith('sqlite'):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={'check_same_thread': False},
+        echo=True
+    )
+else:
+    engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+"""Безопасное закрытие сессии даже при ошибки"""
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
